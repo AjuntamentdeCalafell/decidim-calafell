9bcc25296b982da6660bbe67e191637475fd21d8
Oriol Gual <oriol@codegram.com>
2018-11-20 12:16:29 +0100

    Fix Sidekiq admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 require "sidekiq/web"
 Rails.application.routes.draw do
-  
-  authenticate :user, lambda { |u| u.roles.include?("admin") } do
+  authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
