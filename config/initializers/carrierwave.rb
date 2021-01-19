e3d8003ca7fd1e3b83bb07f512566df0c168ceed
Oriol Gual <oriol@codegram.com>
2021-01-19 11:36:37 +0100

    Update carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,33 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  require "carrierwave/storage/fog"
+# Default CarrierWave setup.
+#
+CarrierWave.configure do |config|
+  config.permissions = 0o666
+  config.directory_permissions = 0o777
+  config.storage = :file
+  config.enable_processing = !Rails.env.test?
+end
 
+# Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
+#
+if Rails.application.secrets.aws_access_key_id.present?
+  require "carrierwave/storage/fog"
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'                                             # required
     config.fog_credentials = {
       provider:              'AWS',                                             # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,       # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,   # required
-      region:                'eu-central-1',                                    # optional, defaults to 'us-east-1'
-      host:                  'decidim-calafell.s3.eu-central-1.amazonaws.com'
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+      region:                Rails.application.secrets.aws_region,
+      host:                  Rails.application.secrets.aws_host,
     }
-    config.fog_directory  = 'decidim-calafell'                                  # required
-    config.fog_public     = true # optional, defaults to true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
-  end
-else
-  CarrierWave.configure do |config|
-    config.permissions = 0o666
-    config.directory_permissions = 0o777
-    config.storage = :file
-    config.enable_processing = !Rails.env.test?
+    config.fog_directory  = Rails.application.secrets.aws_bucket
+    config.fog_public = true
+    config.fog_attributes = {
+      'Cache-Control' => "max-age=#{365.day.to_i}",
+      'X-Content-Type-Options' => "nosniff"
+    }
   end
 end
