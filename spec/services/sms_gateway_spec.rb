18e698065e6d4faaaa7fdef65c06d1e50204416a
leio10 <leiodd@gmail.com>
2021-10-06 12:54:29 +0200

    Update SMS tests

--- spec/services/sms_gateway_spec.rb
+++ spec/services/sms_gateway_spec.rb
@@ -12,12 +12,12 @@ describe SmsGateway do
     before do
       allow(gateway)
         .to receive(:response)
-        .and_return(Nokogiri::XML("<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><enviarSms2Response xmlns=\"http://envios.rbs.innovatelecom.org\"><enviarSms2Return>&lt;ResultadoOp&gt;
+        .and_return(Nokogiri::XML("<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><enviosms2Response xmlns=\"http://envios.rbs.innovatelecom.org\"><enviosms2Return>&lt;ResultadoOp&gt;
   &lt;estado&gt;OK&lt;/estado&gt;
   &lt;codigo&gt;OK&lt;/codigo&gt;
   &lt;detalle&gt;Operaci&#xF3;n realizada con &#xE9;xito&lt;/detalle&gt;
   &lt;identificador&gt;Switch_RBS4SMS_MT_9d2d42d4a8f845e48c6f5fc20d589bba&lt;/identificador&gt;
-&lt;/ResultadoOp&gt;</enviarSms2Return></enviarSms2Response></soapenv:Body></soapenv:Envelope>"))
+&lt;/ResultadoOp&gt;</enviosms2Return></enviosms2Response></soapenv:Body></soapenv:Envelope>"))
     end
 
     describe "deliver_code" do
@@ -37,12 +37,12 @@ describe SmsGateway do
     before do
       allow(gateway)
         .to receive(:response)
-        .and_return(Nokogiri::XML("<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><enviarSms2Response xmlns=\"http://envios.rbs.innovatelecom.org\"><enviarSms2Return>&lt;ResultadoOp&gt;
+        .and_return(Nokogiri::XML("<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><enviosms2Response xmlns=\"http://envios.rbs.innovatelecom.org\"><enviosms2Return>&lt;ResultadoOp&gt;
   &lt;estado&gt;NOK&lt;/estado&gt;
   &lt;codigo&gt;OK&lt;/codigo&gt;
   &lt;detalle&gt;Operaci&#xF3;n realizada con &#xE9;xito&lt;/detalle&gt;
   &lt;identificador&gt;Switch_RBS4SMS_MT_9d2d42d4a8f845e48c6f5fc20d589bba&lt;/identificador&gt;
-&lt;/ResultadoOp&gt;</enviarSms2Return></enviarSms2Response></soapenv:Body></soapenv:Envelope>"))
+&lt;/ResultadoOp&gt;</enviosms2Return></enviosms2Response></soapenv:Body></soapenv:Envelope>"))
     end
 
     describe "deliver_code" do
