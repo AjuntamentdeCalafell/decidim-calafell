b26a9429ee504ea8c7c8e4becef9dda5b0250f1c
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2023-09-08 11:58:11 +0200

    WIP: Fix when phone number is empty and when invoked with a logged in user

--- decidim-verifications-sms_direct/app/packs/src/decidim/verifications/sms_direct/application.js
+++ decidim-verifications-sms_direct/app/packs/src/decidim/verifications/sms_direct/application.js
@@ -1,26 +1,31 @@
 $(() => {
-  const mobilePhoneInput = document.querySelectorAll('#authorization-handler-sms_direct input')[0];
-  const verificationCodeInput = document.querySelectorAll('#authorization-handler-sms_direct input')[1];
+  const mobilePhoneInput = document.querySelectorAll('#sms_direct-form-partial input')[0];
+  const verificationCodeInput = document.querySelectorAll('#sms_direct-form-partial input')[1];
   verificationCodeInput.disabled = true;
 
   const sendSmsCodeButton = document.querySelector('.send-sms-code');
-  
   sendSmsCodeButton.addEventListener('click', function() {
-    sendSmsCodeButton.style.display = 'none';
-    mobilePhoneInput.readOnly = true;
-
     let mobilePhoneNumber = mobilePhoneInput.value;
 
     let request = new XMLHttpRequest();
     var url = `/verifications_sms_direct/sms_codes?phone_num=${mobilePhoneNumber}`;
     request.open("POST", url, true);
  
-    request.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        verificationCodeInput.disabled = false;
+    request.onload = function () {
+      if (request.readyState == "4") {//Request finished and response is ready
+        if (request.status == "200") {
+          sendSmsCodeButton.style.display = 'none';
+          mobilePhoneInput.readOnly = true;
+          verificationCodeInput.disabled = false;
+        } else if (this.status == 400) {
+          mobilePhoneInput.readOnly = false
+          errorMsg = request.responseText;
+          alert(errorMsg);
+        } else {
+          alert("Problem retrieving data");
+        }
       }
     }
-  
     request.send();
   });
 });
