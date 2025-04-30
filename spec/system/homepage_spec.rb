f89c95dcb9cc774e1df958ae6f7699d507cbb7f2
Lucas <lucas.gv@coditramuntana.com>
2025-04-30 12:04:26 +0200

    change value text - renders the home page

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -13,6 +13,6 @@ describe 'Visit the home page', type: :system, perform_enqueued: true do
 
   it 'renders the home page' do
     visit decidim.root_path
-    expect(page).to have_content(/Bienvenida a la plataforma participativa/i)
+    expect(page).to have_content(/Welcome to Schultz LLC participatory platform/i)
   end
 end
