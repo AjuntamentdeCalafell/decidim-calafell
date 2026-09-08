6fc7bbfb42459802be90d05d443881df19daa060
Laura Jaime <laura.jv@coditramuntana.com>
2026-09-08 09:18:58 +0200

    Add rootsys.dev to CSP

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -78,7 +78,8 @@ Decidim.configure do |config|
   # rubocop: disable Lint/PercentStringArray
   config.content_security_policies_extra = {
     "img-src" => %w('self' data: https://*.s3.eu-west-3.amazonaws.com),
-    "connect-src" => %w('self' https://*.s3.eu-west-3.amazonaws.com)
+    "connect-src" => %w('self' https://*.s3.eu-west-3.amazonaws.com),
+    "frame-src" => %w('self' https://rootsys.dev/.zetta/*),
   }
   # rubocop: enable Lint/PercentStringArray
 
